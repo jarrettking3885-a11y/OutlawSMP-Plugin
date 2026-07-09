54150d469727298cf9b9a4e03b797a1e03453e23
jarrettking3885-a11y <jarrettking3885@gmail.com>
2026-07-09 10:23:38 -0500

    Update PlayerManager.java

--- OutlawSMP-Complete (3)/OutlawSMP/src/main/java/com/outlawsmp/managers/PlayerManager.java
+++ OutlawSMP-Complete (3)/OutlawSMP/src/main/java/com/outlawsmp/managers/PlayerManager.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
