4a5a9cb85ef95a48addf047887eae93d3185e733
jarrettking3885-a11y <jarrettking3885@gmail.com>
2026-07-10 03:28:00 -0500

    Add CrystalPickupListener to event registration

--- OutlawSMP-Complete (3)/OutlawSMP/src/main/java/com/outlawsmp/OutlawSMP.java
+++ OutlawSMP-Complete (3)/OutlawSMP/src/main/java/com/outlawsmp/OutlawSMP.java
@@ -1,5 +1,5 @@
 package com.outlawsmp;
-
+ 
 import com.outlawsmp.commands.BalanceCommand;
 import com.outlawsmp.commands.BountyCommand;
 import com.outlawsmp.commands.HunterCommand;
@@ -7,30 +7,31 @@ import com.outlawsmp.commands.OutlawAdminCommand;
 import com.outlawsmp.commands.PayCommand;
 import com.outlawsmp.commands.StatsCommand;
 import com.outlawsmp.commands.WishCommand;
-
+ 
 import com.outlawsmp.database.DatabaseManager;
-
+ 
 import com.outlawsmp.gui.BlessingsGUI;
-
+ 
 import com.outlawsmp.hooks.PlaceholderAPIHook;
-
+ 
+import com.outlawsmp.listeners.CrystalPickupListener;
 import com.outlawsmp.listeners.PlayerDeathListener;
 import com.outlawsmp.listeners.PlayerInteractListener;
 import com.outlawsmp.listeners.PlayerJoinListener;
 import com.outlawsmp.listeners.PlayerQuitListener;
-
+ 
 import com.outlawsmp.managers.BountyManager;
 import com.outlawsmp.managers.CrystalManager;
 import com.outlawsmp.managers.EconomyManager;
 import com.outlawsmp.managers.HunterManager;
 import com.outlawsmp.managers.PlayerManager;
 import com.outlawsmp.managers.WishManager;
-
+ 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
+ 
 public class OutlawSMP extends JavaPlugin {
-
+ 
     private DatabaseManager databaseManager;
     private WishManager wishManager;
     private EconomyManager economyManager;
@@ -39,14 +40,14 @@ public class OutlawSMP extends JavaPlugin {
     private HunterManager hunterManager;
     private BlessingsGUI blessingsGUI;
     private CrystalManager crystalManager;
-
+ 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-
+ 
         databaseManager = new DatabaseManager(this, getConfig().getString("database.file", "outlawsmp.db"));
         databaseManager.connect();
-
+ 
         wishManager = new WishManager(this);
         economyManager = new EconomyManager();
         bountyManager = new BountyManager(this, economyManager, databaseManager);
@@ -54,12 +55,13 @@ public class OutlawSMP extends JavaPlugin {
         hunterManager = new HunterManager(this);
         blessingsGUI = new BlessingsGUI(this);
         crystalManager = new CrystalManager(this);
-
+ 
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
-
+        getServer().getPluginManager().registerEvents(new CrystalPickupListener(this), this);
+ 
         registerCommand("wish", new WishCommand(this));
         registerCommand("balance", new BalanceCommand(this));
         registerCommand("bounty", new BountyCommand(this));
@@ -67,19 +69,19 @@ public class OutlawSMP extends JavaPlugin {
         registerCommand("hunter", new HunterCommand(this));
         registerCommand("outlawadmin", new OutlawAdminCommand(this));
         registerCommand("pay", new PayCommand(this));
-
+ 
         hunterManager.startScheduler();
         crystalManager.start();
-
+ 
         // PlaceholderAPI Hook
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new PlaceholderAPIHook(this).register();
             getLogger().info("PlaceholderAPI hook registered!");
         }
-
+ 
         getLogger().info("OutlawSMP enabled!");
     }
-
+ 
     @Override
     public void onDisable() {
         if (hunterManager != null) {
@@ -98,7 +100,7 @@ public class OutlawSMP extends JavaPlugin {
         }
         getLogger().info("OutlawSMP disabled!");
     }
-
+ 
     private void registerCommand(String name, org.bukkit.command.CommandExecutor executor) {
         org.bukkit.command.PluginCommand command = getCommand(name);
         if (command == null) {
@@ -110,35 +112,35 @@ public class OutlawSMP extends JavaPlugin {
             command.setTabCompleter(tabCompleter);
         }
     }
-
+ 
     public DatabaseManager getDatabaseManager() {
         return databaseManager;
     }
-
+ 
     public WishManager getWishManager() {
         return wishManager;
     }
-
+ 
     public EconomyManager getEconomyManager() {
         return economyManager;
     }
-
+ 
     public BountyManager getBountyManager() {
         return bountyManager;
     }
-
+ 
     public PlayerManager getPlayerManager() {
         return playerManager;
     }
-
+ 
     public HunterManager getHunterManager() {
         return hunterManager;
     }
-
+ 
     public BlessingsGUI getBlessingsGUI() {
         return blessingsGUI;
     }
-
+ 
     public CrystalManager getCrystalManager() {
         return crystalManager;
     }
